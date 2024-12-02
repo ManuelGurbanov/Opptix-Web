e076f36d0a3a5af5752fdd5f0365b5102b5180d6
Manuel Gurbanov <manuelgurbanov@gmail.com>
2024-12-02 18:45:06 -0300

    improved packs responsive

--- src/Packs.js
+++ src/Packs.js
@@ -52,24 +52,28 @@ function Packs() {
     ];
 
     return (
-        <>
+        <div className='min-h-screen w-full items-center flex-col flex'>
         <h1 className='text-center w-full text-4xl font-bold mt-12'>Packs</h1>
-        <h2 className='text-center w-full text-xl opacity-70'>Lorem Ipsum...</h2>
-        <div className='w-1/2 bg-white flex flex-col sm:flex-row justify-center items-start text-black sm:p-12 gap-5 m-0 mb-16 sm:mb-0'>
-            {packs.map((pack, index) => (
-                <div key={index} className='w-full h-[60vh] bg-zinc-200 rounded-xl flex flex-col items-center justify-between text-center p-8 hover:scale-105 transition-all ease-in-out'>
+        <h2 className='text-center w-full text-xl opacity-70 mb-6 sm:mb-0'>Lorem Ipsum...</h2>
+        <div className='w-3/4 sm:w-2/3 min-h-[70vh] sm:min-w-[1100px] bg-white flex flex-col sm:flex-row justify-center items-start text-black sm:p-8 gap-8 m-0 mb-16 sm:mb-0'>
+        {packs.map((pack, index) => (
+                <div key={index} className='relative w-full sm:min-h-[60vh] min-h-[80vh] bg-zinc-200 rounded-xl flex flex-col items-center justify-end text-center p-8 hover:scale-105 transition-all ease-in-out'>
                     <h1 className='text-2xl font-bold mb-2'>{pack.title}</h1>
-                    <p className='text-xl text-zinc-600 mb-2'>{pack.description}</p>
-                    <h2 className='text-3xl font-bold'>${pack.price}</h2>
-                    {pack.monthly && <p className='text-md mb-4'>mensuales</p>}
+                    <p className='text-lg text-zinc-600 mb-2'>{pack.description}</p>
+                    <h2 className='text-4xl font-bold'>${pack.price}</h2>
+                    <p className={`text-md mb-4 transition-opacity duration-500 ${pack.monthly ? 'opacity-100' : 'opacity-0'}`}>
+                        mensuales
+                        </p>
+
 
                     <h3 className='text-lg font-bold text-left w-full'>Resultados</h3>
-                    <ul className='mb-4 text-left text-xs'>
+                    <ul className='mb-4 text-left text-md'>
                         {pack.results.map((result, i) => (
                             <li key={i}>{result}</li>
                         ))}
                     </ul>
-                    <button className='bg-black p-3 text-white rounded-2xl w-2/3 text-sm mb-6 hover:bg-zinc-800 transition-colors ease-in-out duration-75'>Reserva una Demostración</button>
+                    <button className=' bg-black p-3 text-white rounded-2xl min-w-2/3 text-sm justify-self-end mb-6 hover:bg-zinc-800 transition-colors ease-in-out duration-75'>Reserva una Demostración</button>
+                    <p className='text-xs text-center w-1/2'>O envíenos un mensaje con sus preguntas</p>
                 </div>
             ))}
         </div>
@@ -77,7 +81,7 @@ function Packs() {
             <h1 className='text-black font-extrabold text-xl'>Creamos tu Propio Pack</h1>
             <button className='bg-zinc-300 p-3 rounded-xl text-md font-semibold hover:scale-105 transition-all duration-100'>Hablar con un Asesor</button>
         </div>
-        </>
+        </div>
     );
 }
 
