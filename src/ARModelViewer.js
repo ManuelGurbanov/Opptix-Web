8fb8e62a13c0fe0dcadf9ca3400666de5e35e433
Manuel Gurbanov <manuelgurbanov@gmail.com>
2024-12-04 01:32:51 -0300

    finished rack configurator

--- src/ARModelViewer.js
+++ src/ARModelViewer.js
@@ -5,6 +5,7 @@ const ARModelViewer = ({ modelSrc, controlsContainerId }) => {
   const modelViewerRef = useRef(null);
   const [materialGroups, setMaterialGroups] = useState({});
   const [currentGroup, setCurrentGroup] = useState(2);
+  const [menuState, setMenuState] = useState(0);
 
   useEffect(() => {
     const modelViewer = modelViewerRef.current;
@@ -41,6 +42,8 @@ const ARModelViewer = ({ modelSrc, controlsContainerId }) => {
 
       const variantSelect = document.createElement("select");
       variantSelect.id = "variant";
+      variantSelect.style.display = "none";
+      variantSelect.className = "text-zinc-700 bg-zinc-200 rounded px-4 py-2 m-2 rounded-xl ring-1 ring-black hover:scale-105 transition-all duration-75 hover:ring-blue-500";
       names.forEach((name) => {
         const option = document.createElement("option");
         option.value = name;
@@ -56,15 +59,45 @@ const ARModelViewer = ({ modelSrc, controlsContainerId }) => {
       variantSelect.addEventListener("input", async (event) => {
         const selectedVariant = event.target.value === "default" ? null : event.target.value;
         modelViewer.variantName = selectedVariant;
-        await updateGroups(materialGroups, currentGroup);
+        await modelViewer.model.updateComplete;
+        const variantChangeEvent = new Event("variantchange");
+        modelViewer.dispatchEvent(variantChangeEvent);
       });
+      
+      const backButton = document.createElement("button");
+      backButton.textContent = "Volver";
+      backButton.style.display = "none";
+      backButton.className = "text-zinc-700 bg-red-200 rounded px-4 py-2 m-2 rounded-xl ring-1 ring-black hover:scale-105 transition-all duration-75 hover:ring-blue-500";
+      backButton.addEventListener("click", () => {
+        setMenuState(0);
+        controlsContainer.className = "flex items-center justify-center bg-transparent p-4 rounded absolute bottom-0 gap-2 w-full"
+        variantSelect.style.display = "none";
+        backButton.style.display = "none";
+        configureButton.style.display = "inline-block";
+        reduceButton.style.display = "none";
+        expandButton.style.display = "none";
+      })
 
-      controlsContainer.appendChild(variantSelect);
+      const configureButton = document.createElement("button");
+      configureButton.textContent = "Configurar";
+      configureButton.className = "text-zinc-700 bg-zinc-200 rounded px-4 py-2 m-2 rounded-xl ring-1 ring-black hover:scale-105 transition-all duration-75 hover:ring-blue-500";
+      configureButton.addEventListener("click", () => {
+        setMenuState(1);
+        backButton.style.display = "block";
+        controlsContainer.className = "grid grid-cols-2 bg-transparent p-4 rounded absolute bottom-0 gap-2"
+        configureButton.style.display = "none";
+        variantSelect.style.display = "block";
+        reduceButton.style.display = "inline-block";
+        expandButton.style.display = "inline-block";
+      });
+      controlsContainer.appendChild(backButton);
+      controlsContainer.appendChild(configureButton);
 
       const reduceButton = document.createElement("button");
       reduceButton.textContent = "Reducir";
-      reduceButton.className = "text-white bg-red-500 rounded px-4 py-2 m-2";
-      reduceButton.addEventListener("click", async () => {
+      reduceButton.className = "text-zinc-700 bg-zinc-200 rounded px-4 py-2 m-2 rounded-xl ring-1 ring-black hover:scale-105 transition-all duration-75 hover:ring-blue-500";
+      reduceButton.style.display = "none";
+      reduceButton.addEventListener("click", () => {
         setCurrentGroup((prev) => {
           const newGroup = Math.max(prev - 1, 0);
           updateGroups(materialGroups, newGroup);
@@ -74,8 +107,9 @@ const ARModelViewer = ({ modelSrc, controlsContainerId }) => {
 
       const expandButton = document.createElement("button");
       expandButton.textContent = "Ampliar";
-      expandButton.className = "text-white bg-green-500 rounded px-4 py-2 m-2";
-      expandButton.addEventListener("click", async () => {
+      expandButton.className = "text-zinc-700 bg-zinc-200 rounded px-4 py-2 m-2 rounded-xl ring-1 ring-black hover:scale-105 transition-all duration-75 hover:ring-blue-500";
+      expandButton.style.display = "none";
+      expandButton.addEventListener("click", () => {
         setCurrentGroup((prev) => {
           const newGroup = Math.min(prev + 1, 6);
           updateGroups(materialGroups, newGroup);
@@ -83,6 +117,7 @@ const ARModelViewer = ({ modelSrc, controlsContainerId }) => {
         });
       });
 
+      controlsContainer.appendChild(variantSelect);
       controlsContainer.appendChild(reduceButton);
       controlsContainer.appendChild(expandButton);
     };
@@ -94,74 +129,52 @@ const ARModelViewer = ({ modelSrc, controlsContainerId }) => {
     };
   }, [controlsContainerId]);
 
-
   const updateGroups = async (groups, currentGroup) => {
     const modelViewer = modelViewerRef.current;
     if (!modelViewer) return;
-  
-    // Obtener todos los materiales actuales, incluidos los de la variante activa.
+
     const allMaterials = modelViewer.model.materials;
-  
+
     for (const material of allMaterials) {
       await material.ensureLoaded();
-  
+
       const prefix = material.name.slice(0, 2);
       const groupIndex = parseInt(prefix, 10);
       const isActive = groupIndex <= currentGroup;
-  
+
       material.setAlphaMode("BLEND");
       const pbr = material.pbrMetallicRoughness;
       const baseColor = pbr.baseColorFactor;
       const newOpacity = isActive ? 1 : 0;
-  
-      // Actualizar la opacidad del material.
+
       baseColor[3] = newOpacity;
       pbr.setBaseColorFactor(baseColor);
-  
-      // Registrar la acción realizada.
-      console.log(`Al material "${material.name}" le dejé opacidad ${newOpacity}`);
+      //console.log(Al material "${material.name}" le dejé opacidad ${newOpacity});
     }
   };
-  
-  
+
   useEffect(() => {
     const modelViewer = modelViewerRef.current;
     if (!modelViewer) return;
-  
+
     const handleVariantChange = async () => {
       console.log("Cambiando variante...");
       await modelViewer.model.updateComplete;
-  
-      await updateGroups(materialGroups, currentGroup);
+      console.log("Variante cambiada. Llamando a updateGroups...");
+      console.log('materialGroups ' + materialGroups);
+      console.log('currentGroup ' +  currentGroup);
+      updateGroups(materialGroups, currentGroup);
     };
-  
+
     modelViewer.addEventListener("variantchange", handleVariantChange);
-  
+
     return () => {
       modelViewer.removeEventListener("variantchange", handleVariantChange);
     };
   }, [currentGroup, materialGroups]);
-  
-  
-  useEffect(() => {
-    const modelViewer = modelViewerRef.current;
-    if (!modelViewer) return;
-  
-    const handleVariantChange = async () => {
-      console.log(modelViewer.model.materials);
-      await modelViewer.model.updateComplete;
-      await updateGroups(materialGroups, currentGroup);
-    };
-  
-    modelViewer.addEventListener("variantchange", handleVariantChange);
-  
-    return () => {
-      modelViewer.removeEventListener("variantchange", handleVariantChange);
-    };
-  }, [currentGroup, materialGroups]);
-  
+
   return (
-    <div className="relative flex items-center justify-center w-full h-full">
+    <div className="relative flex items-center justify-center w-full h-full bg-white">
       <model-viewer
         ref={modelViewerRef}
         src={modelSrc}
--- src/OurServices.js
+++ src/OurServices.js
@@ -72,14 +72,13 @@ function OurServices({ language }) {
         ))}
       </div>
 
-      <section className="flex full rounded-lg flex-col sm:flex-row p-8">
-        <div className="bg-zinc-600 w-full sm:w-[422px] sm:h-[502px] aspect-square sm:aspect-auto ring-2 ring-black">
-          {/* <ARModelViewer modelSrc="/models/rack-modulable.glb" controlsContainerId="material-controls-own"
-          /> */}
+      <section className="flex rounded-lg flex-col sm:flex-row ring-2 sm:ring-zinc-400 mt-4 p-0 sm:p-8">
+        <div className="bg-zinc-600 w-full sm:w-[422px] sm:h-[502px] aspect-square sm:aspect-auto relative">
           <ARModelViewer modelSrc='/models/rack-final.glb' controlsContainerId="material-controls-own" whiteWoodTextureUrl={"https://t4.ftcdn.net/jpg/01/66/54/45/360_F_166544573_4XQicLyGnsCvKZi2KifqFqPpu73DYLPT.jpg"} />
+          <div id="material-controls-own" className="bg-transparent p-4 rounded absolute bottom-0 gap-2"></div>
         </div>
         
-        <div className="bg-white w-full sm:w-[448px] sm:h-[502px] aspect-square sm:aspect-auto p-7 ring-2 ring-black flex flex-col justify-between">
+        <div className="bg-white w-full sm:w-[448px] sm:h-[502px] aspect-square sm:aspect-auto p-7 flex flex-col justify-between">
           <div>
             <h1 className="font-bold text-lg w-full text-left mt-3 mb-3">
               {services[actualService].name}
@@ -89,7 +88,7 @@ function OurServices({ language }) {
             </p>
           </div>
           
-          <div id="material-controls-own" className="mt-4 bg-gray-100 p-4 rounded"></div>
+          
         </div>
       </section>
 
