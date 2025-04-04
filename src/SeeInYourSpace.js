d12f440b5747faba4bff1afa8af4f02aa64f6b9d
Manuel Gurbanov <manuelgurbanov@gmail.com>
2025-04-04 14:53:00 -0300

    replaced visagra

--- src/SeeInYourSpace.js
+++ src/SeeInYourSpace.js
@@ -6,7 +6,7 @@ export default function SeeButton ({language, qrCode = "/qrcodes/bike.png"}) {
     const [showingQR, setShowingQR] = useState(false);
 
     return (
-        <div className="flex flex-col items-center justify-center gap-4">
+        <div className="flex flex-col items-center justify-center gap-4 relative">
         <button className="px-2 py-1 text-black transition-all rounded-full bg-lightblue6 hover:bg-lightblue2 hover:text-white flex items-center justify-center h-12" onClick={() => setShowingQR(!showingQR)}>
             <img className="w-8 mr-2 ml-2" src="/ARsvg.svg" alt="QR Code"></img>
            <p className="mr-4 font-semibold"> {language === "es" ? "Ver en tu espacio" : "See in your space"}
@@ -14,7 +14,7 @@ export default function SeeButton ({language, qrCode = "/qrcodes/bike.png"}) {
         </button>
 
         {showingQR &&
-        <div className="flex flex-col items-center justify-center gap-4 bg-lightblue2 bg-opacity-75 rounded-3xl p-4 w-48">
+        <div className="flex flex-col items-center justify-center gap-4 bg-lightblue2 bg-opacity-75 rounded-3xl p-4 w-48 absolute -bottom-[200px] z-30">
 
         <img className="w-full" src={qrCode} alt="QR Code">
         </img>
